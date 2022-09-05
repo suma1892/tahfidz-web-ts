7a398311d03ddb0bd44fd2fab0ed6057a3d5a39b
suma1892 <sumaalbaroh1892@gmail.com>
2022-09-05 13:40:52 +0700

    fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
-import { NextPage } from 'next';
-import Image from 'next/image';
-import React from 'react';
+import { NextPage } from "next";
+import Image from "next/image";
+import React from "react";
 
 const Footer: NextPage = () => {
   return (
-    <footer className="footer" id='footer'>
+    <footer className="footer" id="footer">
       <div className="container">
         <div className="footer__row">
           <div className="footer__col-1">
@@ -12,8 +12,8 @@ const Footer: NextPage = () => {
               <Image
                 src="/icons/whiteTahfidz.png"
                 alt="logo image"
-                layout='fill'
-                objectFit='cover'
+                layout="fill"
+                objectFit="cover"
               />
             </div>
             <p>{`Yayasan Istiqomah Tarbiyah Sunnah adalah suatu lembaga yang bergerak di bidang dakwah dan pendidikan Islam berlandaskan Al-qur'an dan As-sunnah di Kota Banjar`}</p>
@@ -23,9 +23,24 @@ const Footer: NextPage = () => {
               <h3>Kontak Kami</h3>
             </div>
             <div className="footer__contact--items">
-              <a href='https://api.whatsapp.com/send?phone=6287870378484&' target="_blank" rel="noreferrer">WhatsApp: 087870378484</a>
-              <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=istiqomahtarbiyahsunnah@gmail.com&tf=1" target="_blank" rel="noreferrer">Email: istiqomahtarbiyahsunnah@gmail.com</a>
-              <p>Alamat: Jl. Dewisartika, No. 81, Parung Lesang, Kelurahan Banjar, Kecamatan Banjar, Kota Banjar, 46322, Jawa Barat</p>
+              <a
+                href="https://api.whatsapp.com/send?phone=6287870378484&"
+                target="_blank"
+                rel="noreferrer"
+              >
+                WhatsApp: 087870378484
+              </a>
+              <a
+                href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=istiqomahtarbiyahsunnah@gmail.com&tf=1"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Email: istiqomahtarbiyahsunnah@gmail.com
+              </a>
+              <p>
+                Alamat: Jl. Dewisartika, No. 81, Parung Lesang, Kelurahan
+                Banjar, Kecamatan Banjar, Kota Banjar, 46322, Jawa Barat
+              </p>
               <p>Buka setiap hari senin - jumat mulai pukul 7:00 - 17:00</p>
             </div>
           </div>
